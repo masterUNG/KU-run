da81c5677f8c63ca3e002a2327526a276359b5d0
masterUNG <phrombutr@gmail.com>
2016-03-08 15:48:33 +0700

    Syn Finish

--- app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/MainActivity.java
+++ app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/MainActivity.java
@@ -2,9 +2,23 @@ package kusrc.worapong.preyapron.sriwan.kurun;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +40,92 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQlite
         deleteAllSQLite();
 
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
 
     }   // Main Method
+
+    private void synJSONtoSQLite() {
+
+        //Connected Http
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        int intTABLE = 0;
+        String[] urlTABLEStrings = {"http://swiftcodingthai.com/keng/php_get_user_master.php"};
+
+        while (intTABLE <= 0) {
+
+            //1 Create InputStream
+            InputStream inputStream = null;
+            try {
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(urlTABLEStrings[intTABLE]);
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                inputStream = httpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("KuRun", "InputStream ==> " + e.toString());
+            }
+
+            //2 Create JSON String
+            String strJSON = null;
+            try {
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                StringBuilder stringBuilder = new StringBuilder();
+                String strLine = null;
+                while ((strLine = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(strLine);
+                }
+                inputStream.close();
+                strJSON = stringBuilder.toString();
+
+            } catch (Exception e) {
+                Log.d("KuRun", "JSON String ==> " + e.toString());
+            }
+
+            //3 Update SQLite
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    switch (intTABLE) {
+                        case 0:
+
+                            String strName = jsonObject.getString(MyManage.column_Name);
+                            String strSurname = jsonObject.getString(MyManage.column_Surname);
+                            String strIDstudent = jsonObject.getString(MyManage.column_ID_Student);
+                            String strYear = jsonObject.getString(MyManage.column_Year);
+                            String strUser = jsonObject.getString(MyManage.column_User);
+                            String strPassword = jsonObject.getString(MyManage.column_Password);
+                            String strAvata = jsonObject.getString(MyManage.column_Avata);
+
+                            myManage.addUser(strName, strSurname, strIDstudent,
+                                    strYear, strUser, strPassword, strAvata);
+
+                            break;
+                    }   // switch
+                }   // for
+
+            } catch (Exception e) {
+                Log.d("KuRun", "UPdate ==> " + e.toString());
+            }
+
+
+
+            intTABLE += 1;
+        }   // while
+
+
+    }   // synJSON
 
     private void deleteAllSQLite() {
         SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
@@ -39,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
         myManage.addUser("name", "sur", "id", "2016", "user", "12345", "3");
     }
 
-    public void clickSignUpMain(View view){
+    public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 }   // Main Class
